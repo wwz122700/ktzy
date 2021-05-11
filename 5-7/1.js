196fa9fe6e15179f883419f4f8af8798c31ba23e
2009 <3106919422@qq.com>
2021-05-11 17:00:41 +0800

    class-module

---
+++ 5-7/1.js
@@ -0,0 +1,15 @@
+let name="lisi"
+let mm="傻子"
+let obj1={
+    name:"张三",
+    age:30,
+    sex:"男"
+}
+function k(){
+    pwd:"密码"
+}
+let num=function(){
+    username="姓名"
+    age=40
+}
+export{name,obj1,num,k,mm}
---
+++ 5-7/2.js
@@ -0,0 +1,3 @@
+import{name,obj1,num,k,mm} from "./1.js"
+// mm
+// console.log(name,obj1,num,k,mm)
